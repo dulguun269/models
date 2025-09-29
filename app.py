30df95c99d1b9407db9185c97ab704b87f4079a6
dulguun <dulguun.b@numur.mn>
2025-09-29 16:38:57 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from your_models_lib import db, Person
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return "✅ Flask app running with shared models!"
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=3000)
